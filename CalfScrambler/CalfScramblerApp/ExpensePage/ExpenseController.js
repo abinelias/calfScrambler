51196eeec0ae8eea02dcf0edeaf4c94ff13690f1
Abin Abraham <abinsilpa@gmail.com>
2018-06-07 19:35:56 +0530

    Refreshing iisue

--- CalfScrambler/CalfScramblerApp/ExpensePage/ExpenseController.js
+++ CalfScrambler/CalfScramblerApp/ExpensePage/ExpenseController.js
@@ -11,7 +11,7 @@
         vm.selectedQuanity = 0;
         vm.expenseType = '';
         vm.unitCost;
-        vm.monthlyData = [{ "ExpenseType": "Feed", "Quantity": 10, "UnitCost": "$30", "TotalCost": "$300" }];
+        vm.monthlyData = [{ "Id": 1, "ExpenseType": "Feed", "Quantity": 10, "UnitCost": "$30", "TotalCost": "$300" }, { "Id": 2, "ExpenseType": "Medicine", "Quantity": 10, "UnitCost": "$30", "TotalCost": "$300" }, { "Id": 3, "ExpenseType": "Food", "Quantity": 10, "UnitCost": "$30", "TotalCost": "$300" }];
         vm.yearlyData =  [{ "ReportingMonth": "May", "ExpenseType": "Feed", "Quantity": 10, "UnitCost": "$30", "TotalCost": "$300" }];
         vm.quantity = [{ "Id": 0, "Name": "Select Quantity"}, { "Id": 1, "Name": 1 }, { "Id": 2, "Name": 2 }, { "Id": 3, "Name": 3 }, { "Id": 4, "Name": 4 }, { "Id": 5, "Name": 5 }, { "Id": 6, "Name": 6 }, { "Id": 7, "Name": 7 }, { "Id": 8, "Name": 8 }, { "Id": 9, "Name": 9 }, { "Id": 10, "Name": 10 }]
 
@@ -45,8 +45,8 @@
             vm.unitCost = '';
         }
 
-        function deleteExpense() {
-            alert('hii');
+        function deleteExpense(id) {
+            alert(id);
             
         }
 
