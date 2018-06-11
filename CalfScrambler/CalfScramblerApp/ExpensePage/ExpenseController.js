d3a79d5339f96d0b9bb697151006b0a92d673bd7
Abin Abraham <abinsilpa@gmail.com>
2018-06-11 07:55:54 +0530

    Adding Expense

--- CalfScrambler/CalfScramblerApp/ExpensePage/ExpenseController.js
+++ CalfScrambler/CalfScramblerApp/ExpensePage/ExpenseController.js
@@ -11,17 +11,22 @@
         vm.selectedQuanity = 0;
         vm.expenseType = '';
         vm.unitCost;
+        vm.AddNewExpense = false;
         vm.monthlyData = [{ "Id": 1, "ExpenseType": "Feed", "Quantity": 10, "UnitCost": "$30", "TotalCost": "$300" }, { "Id": 2, "ExpenseType": "Medicine", "Quantity": 10, "UnitCost": "$30", "TotalCost": "$300" }, { "Id": 3, "ExpenseType": "Food", "Quantity": 10, "UnitCost": "$30", "TotalCost": "$300" }];
         vm.yearlyData =  [{ "ReportingMonth": "May", "ExpenseType": "Feed", "Quantity": 10, "UnitCost": "$30", "TotalCost": "$300" }];
         vm.quantity = [{ "Id": 0, "Name": "Select Quantity"}, { "Id": 1, "Name": 1 }, { "Id": 2, "Name": 2 }, { "Id": 3, "Name": 3 }, { "Id": 4, "Name": 4 }, { "Id": 5, "Name": 5 }, { "Id": 6, "Name": 6 }, { "Id": 7, "Name": 7 }, { "Id": 8, "Name": 8 }, { "Id": 9, "Name": 9 }, { "Id": 10, "Name": 10 }]
 
         // Functions defined on vm
         vm.AssignHolding = AssignHolding;
+        vm.AddExpense = AddExpense;
         vm.ExpenseToExcel = ExpenseToExcel;
         vm.saveExpense = saveExpense;
         vm.cancelExpense = cancelExpense;
         vm.deleteExpense = deleteExpense;
 
+        function AddExpense() {
+            vm.AddNewExpense = true;
+        }
 
         function AssignHolding(accountHolding, index) {
             GoalAccountsService.AssignHolding(accountHolding, vm.GoalAccounts[index], vm.profileText.toLowerCase());
@@ -37,12 +42,14 @@
             vm.selectedQuanity = 0;
             vm.expenseType = '';
             vm.unitCost = '';
+            vm.AddNewExpense = false;
         }
 
         function cancelExpense() {
             vm.selectedQuanity = 0;
             vm.expenseType = '';
             vm.unitCost = '';
+            vm.AddNewExpense = false;
         }
 
         function deleteExpense(id) {
